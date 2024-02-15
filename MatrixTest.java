1ddc233d66d8289f66471dc76ef6d2503983ec86
Fisher48 <rybakov48@yandex.ru>
2024-02-15 19:46:31 +0300

    Changed annotations

--- MatrixTest.java
+++ MatrixTest.java
@@ -1,7 +1,8 @@
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatrixTest {
-    @org.junit.jupiter.api.Test
+    @Test
     public void EqualTest() {
         int [] a = {1,2,3,4,5}; // Эталон
         int [] b = {3,2,4,1,5};
@@ -10,7 +11,7 @@ class MatrixTest {
         assertArrayEquals(a, Matrix.sort(b1));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void NegativeEqualTest() {
         int [] a = {-85,-21,-21,-5,-5,5}; // Эталон
         int [] b = {-5,5,-85,-5,-21,-21};
@@ -19,14 +20,14 @@ class MatrixTest {
         assertArrayEquals(a, Matrix.sort(b1));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void OneNumTest() {
         int [] a = {1};
         int [] b = {1};
         assertArrayEquals(a, Matrix.sort(b));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void SameEqualTest() {
         int [] a = {3,4,5,6,7};
         int [] b = {3,4,5,6,7};
