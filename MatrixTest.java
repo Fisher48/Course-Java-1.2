b2415ac9594e5cf81ea8960b12566113de5a3da4
Fisher48 <rybakov48@yandex.ru>
2024-02-15 19:44:50 +0300

    Added comments

--- MatrixTest.java
+++ MatrixTest.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MatrixTest {
     @org.junit.jupiter.api.Test
     public void EqualTest() {
-        int [] a = {1,2,3,4,5};
+        int [] a = {1,2,3,4,5}; // Эталон
         int [] b = {3,2,4,1,5};
         int [] b1 = {1,5,3,2,4};
         assertArrayEquals(a, Matrix.sort(b));
@@ -12,7 +12,7 @@ class MatrixTest {
 
     @org.junit.jupiter.api.Test
     public void NegativeEqualTest() {
-        int [] a = {-85,-21,-21,-5,-5,5};
+        int [] a = {-85,-21,-21,-5,-5,5}; // Эталон
         int [] b = {-5,5,-85,-5,-21,-21};
         int [] b1 = {-21,-5,5,-21,-85,-5};
         assertArrayEquals(a, Matrix.sort(b));
