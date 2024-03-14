a813c3f57631b803ea759e3af09c393875eef78e
Fisher48 <rybakov48@yandex.ru>
2024-03-14 22:19:37 +0300

    func get ArrayList

--- TenFiles.java
+++ TenFiles.java
@@ -1,37 +1,32 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TenFiles {
-    public static int func(int path, int path2) {
+    public static int func(ArrayList list) {
+        Random rand = new Random();
         Logger log = Logger.getLogger(TenFiles.class.getName());
         BufferedReader br = null;
         int sum = 0;
+        int x = 2;
         try {
-            File my_file = new File(path + ".txt");
-            br = new BufferedReader(new FileReader(my_file));
-            String st = br.readLine();
-            assertNotNull(st, "Файл пустой");
-            while (st != null) {
-                System.out.println(st);
-                sum += Integer.parseInt(st);
-                st = br.readLine();
+            for (int i = 0;  i < x; i++) {
+                int r = rand.nextInt(list.size());
+                File my_file = new File(r + ".txt");
+                br = new BufferedReader(new FileReader(my_file));
+                String st = br.readLine();
+                assertNotNull(st, "Файл пустой");
+                while (st != null) {
+                    System.out.println(st);
+                    sum += Integer.parseInt(st);
+                    st = br.readLine();
+                }
+                br.close();
+                System.out.println("Файл номер - " + r);
             }
-            br.close();
-            System.out.println("Файл номер - " + path);
-
-            File my_file2 = new File(path2 + ".txt");
-            br = new BufferedReader(new FileReader(my_file2));
-            String st2 = br.readLine();
-            assertNotNull(st2, "Файл пустой");
-            while (st2 != null) {
-                System.out.println(st2);
-                sum += Integer.parseInt(st2);
-                st2 = br.readLine();
-            }
-            System.out.println("Файл номер - " + path2);
             return sum;
         } catch (IOException e) {
             System.err.println("Error");
@@ -49,8 +44,9 @@ public class TenFiles {
         public static void main (String[]args) {
             Logger log = Logger.getLogger(TenFiles.class.getName());
             BufferedWriter bw;
+            ArrayList<File> list = new ArrayList<>();
             try {
-                int n = 10;
+                int n = 222;
                 Random rand = new Random();
                 for (int i = 1; i <= n; i++) {
                     File my_fil = new File(i + ".txt");
@@ -61,9 +57,10 @@ public class TenFiles {
                     bw.write(String.valueOf(rand.nextInt(100) + 1));
                     bw.newLine();
                     bw.write(String.valueOf(rand.nextInt(100) + 1));
+                    list.add(my_fil);
                     bw.close();
                 }
-                System.out.println("Сумма из 2-х файлов = " + func(rand.nextInt(10) + 1, rand.nextInt(10) + 1));
+                System.out.println("Сумма из 2-х файлов = " + func(list));
             } catch (Exception e) {
                 log.warning("Ошибка записи файла");
             }
