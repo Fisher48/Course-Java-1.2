c93e54261cc2ea16a40b4b4a4d80f7b90d1ac548
Fisher48 <rybakov48@yandex.ru>
2024-03-15 14:34:42 +0300

    Added new Exceptions

--- TenFiles.java
+++ TenFiles.java
@@ -1,21 +1,25 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TenFiles {
+
+    public static class NotCorrectQuantityofStringException extends Exception {
+        public NotCorrectQuantityofStringException (String message) {
+            super(message);
+        }
+    }
+
     public static int[] filesSum(ArrayList list) {
         Random rand = new Random();
         int[] res = new int[2];
         res[0] = 0;
         res[1] = 0;
         for (int i = 0; i < 2; i++) {
-            int x = rand.nextInt(list.size());
             int[] r;
-            r = func((File) list.get(x));
+            r = func((File) list.get(rand.nextInt(list.size())+1));
             if (r[1] != 0) {
                 res[1] = r[1];
                 return res;
@@ -31,26 +35,36 @@ public class TenFiles {
         int[] res = new int[2];
         res[1] = 0;
         int sum = 0;
+        int count = 0;
         try {
-                File my_file = new File(String.valueOf(file));
-                br = new BufferedReader(new FileReader(my_file));
-                String st = br.readLine();
-                assertNotNull(st, "Файл пустой");
-                while (st != null) {
-                    log.info(st);
-                    sum += Integer.parseInt(st);
-                    st = br.readLine();
-                }
-                log.info("Файл номер - " + file.getName());
+            File my_file = new File(String.valueOf(file));
+            br = new BufferedReader(new FileReader(my_file));
+            String st = br.readLine();
+            assertNotNull(st, "Файл пустой");
+            while (st != null) {
+                log.info(st);
+                sum += Integer.parseInt(st);
+                st = br.readLine();
+                count++;
+            }
+            if (count != 3) {
+                throw new NotCorrectQuantityofStringException ("Неверное кол-во строк");
+            }
+            log.info("Файл номер - " + file.getName());
         } catch (IOException e) {
             log.warning("Error");
-        } catch (Exception e) {
+        } catch (NumberFormatException e) {
             res[1] = 1;
+            log.warning("Файл не парсится");
+        } catch (NotCorrectQuantityofStringException exception) {
+            res[1] = 2;
+            log.warning("Неверное кол-во строк");
         } finally {
             if (br != null) {
                 try {
                     br.close();
                 } catch (IOException e) {
+                    res[1] = 3;
                     log.warning("Ошибка чтения файла");
                 }
             }
