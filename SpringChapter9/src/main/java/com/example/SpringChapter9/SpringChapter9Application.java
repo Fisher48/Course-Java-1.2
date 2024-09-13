c6d4dc0e28a7558b00da82d980e98fe7cb846fe8
Fisher48 <rybakov48@yandex.ru>
2024-09-13 13:17:01 +0300

    create SpringChapter9

---
+++ SpringChapter9/src/main/java/com/example/SpringChapter9/SpringChapter9Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringChapter9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChapter9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChapter9Application.class, args);
+	}
+
+}
---
+++ SpringChapter9/src/main/java/com/example/SpringChapter9/model/LoginProcessor.java
@@ -0,0 +1,53 @@
+package com.example.SpringChapter9.model;
+
+import com.example.SpringChapter9.services.LoggedUserManagementService;
+import com.example.SpringChapter9.services.LoginCountService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+
+    private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
+
+    private String username;
+    private String password;
+
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
+        this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
+    }
+
+    public boolean login() {
+        loginCountService.increment();
+
+        String username = this.getUsername();
+        String password = this.getPassword();
+
+        boolean loginResult = false;
+        if ("fisher".equals(username) && "12345".equals(password)) {
+            loginResult = true;
+            loggedUserManagementService.setUsername(username);
+        }
+        return loginResult;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
